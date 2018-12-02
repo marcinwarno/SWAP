bb33d6535c0f1eda7e60a2cc341a7ae5826bedc3
Marcin Warno <marcin.warno@gmail.com>
2018-12-02 11:44:44 +0100

    Initial Commit

---
+++ SWAP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SWAP
+//
+//  Created by Marcin Warno on 02/12/2018.
+//  Copyright © 2018 Marcin Warno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
