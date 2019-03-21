5515a6c4503f34c9ea349b25813025a779efbabb
Antoni B <antoni.technical@gmail.com>
2019-03-22 00:25:54 +0100

    Add adding snapchat and instagram username to database

--- SWAP/LinkingViewController.swift
+++ SWAP/LinkingViewController.swift
@@ -18,7 +18,10 @@ class LinkingViewController: UIViewController {
     }
     
     @IBOutlet weak var snapchatNick: UITextField!
+<<<<<<< HEAD
     @IBOutlet weak var instagramNick: UITextField!
+=======
+>>>>>>> 6e29caea86e594ad1c3d43d6712023e33943d6ae
     
     @IBAction func snapchatLink(_ sender: UIButton) {
         
@@ -26,6 +29,7 @@ class LinkingViewController: UIViewController {
         
         snapchatNick.isEnabled = false
         
+<<<<<<< HEAD
         let userID = Auth.auth().currentUser?.uid
         
         var ref: DatabaseReference!
@@ -59,6 +63,30 @@ class LinkingViewController: UIViewController {
     }
     
     
+=======
+        let snapchatLinking = Database.database().reference().child("Nicks")
+        
+        let snapchatDictionary = ["Sender": Auth.auth().currentUser?.email,
+                                  "Nick": snapchatNick.text!]
+        
+        snapchatLinking.childByAutoId().setValue(snapchatDictionary) {
+            (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Snapchat linked")
+                
+                self.snapchatNick.isEnabled = true
+                self.snapchatNick.text = ""
+            }
+        }
+        
+        
+        
+    }
+    
+>>>>>>> 6e29caea86e594ad1c3d43d6712023e33943d6ae
     /*
     // MARK: - Navigation
 
