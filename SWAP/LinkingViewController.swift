96e2ff9ec844818c6cb65e340c3c2b1830eb7ee5
Anna Strzeżek <astrzezek@gmail.com>
2019-03-22 14:21:11 +0100

    Remove failed merge git additions

--- SWAP/LinkingViewController.swift
+++ SWAP/LinkingViewController.swift
@@ -18,18 +18,14 @@ class LinkingViewController: UIViewController {
     }
     
     @IBOutlet weak var snapchatNick: UITextField!
-<<<<<<< HEAD
     @IBOutlet weak var instagramNick: UITextField!
-=======
->>>>>>> 6e29caea86e594ad1c3d43d6712023e33943d6ae
-    
+
     @IBAction func snapchatLink(_ sender: UIButton) {
         
         snapchatNick.endEditing(true)
         
         snapchatNick.isEnabled = false
         
-<<<<<<< HEAD
         let userID = Auth.auth().currentUser?.uid
         
         var ref: DatabaseReference!
@@ -61,40 +57,4 @@ class LinkingViewController: UIViewController {
         instagramNick.isEnabled = true
         
     }
-    
-    
-=======
-        let snapchatLinking = Database.database().reference().child("Nicks")
-        
-        let snapchatDictionary = ["Sender": Auth.auth().currentUser?.email,
-                                  "Nick": snapchatNick.text!]
-        
-        snapchatLinking.childByAutoId().setValue(snapchatDictionary) {
-            (error, reference) in
-            
-            if error != nil {
-                print(error!)
-            } else {
-                print("Snapchat linked")
-                
-                self.snapchatNick.isEnabled = true
-                self.snapchatNick.text = ""
-            }
-        }
-        
-        
-        
-    }
-    
->>>>>>> 6e29caea86e594ad1c3d43d6712023e33943d6ae
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
