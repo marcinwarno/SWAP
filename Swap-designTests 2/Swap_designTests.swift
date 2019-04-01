15370b6383708833a8b5b4ffb986808ec79ebcb5
Marcin Warno <marcin.warno@gmail.com>
2019-04-01 10:39:32 +0200

    UI improvements

---
+++ Swap-designTests 2/Swap_designTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swap_designTests.swift
+//  Swap-designTests
+//
+//  Created by Marcin Warno on 28/03/2019.
+//  Copyright © 2019 Marcin Warno. All rights reserved.
+//
+
+import XCTest
+@testable import Swap_design
+
+class Swap_designTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
