7d4ce62b3140eb2045aac943c67b9cf507490492
Marcin Warno <marcin.warno@gmail.com>
2019-04-03 16:51:42 +0200

    Added Qr code generating functionality

---
+++ Swap-design/Controller/EmailLoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EmailLoginViewController.swift
+//  Swap-design
+//
+//  Created by Marcin Warno on 03/04/2019.
+//  Copyright © 2019 Marcin Warno. All rights reserved.
+//
+
+import UIKit
+
+class EmailLoginViewController: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
